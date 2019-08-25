91ebdbf51bc760bf4c2ce48838fc67b95f6bb627
eys <eys@google.com>
2019-08-25 15:04:14 +0200

    vector edit

--- Vector.js
+++ Vector.js
@@ -3,35 +3,41 @@ class Vector {
         this.x = x
         this.y = y
     }
+
     negate() {
         return new Vector(-this.x, -this.y)
     }
+
     add(vector) {
-        if (!vector) return 'err: no vector specified'
         return new Vector(this.x + vector.x, this.y + vector.y)
     }
+
     subtract(vector) {
         return this.add(vector.negate())
     }
+
     multiply(scalar=1) {
         return new Vector(this.x * scalar, this.y * scalar)
     }
+
     divide(scalar=1) {
-        if (scalar === 0) return new Vector()
-        else              return this.multiply(1/scalar)
+        return this.multiply(1/scalar)
     }
+
     dot(vector) {
         return this.x * vector.x + this.y * vector.y
     }
+
     length() {
         return Math.sqrt(this.dot(this))
     }
+
     normalize() {
         return this.divide(this.length())
     }
+
+    equals(vector) {
+        return (this.x === vector.x && this.y === vector.y)
+    }
 }
 
-
-/* -------------------------------------------------------------------------------- */
-
-const vec = new Vector()
