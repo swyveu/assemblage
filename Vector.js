3f3882dceab2d3ec1ae2811b8f8f177a03d1add4
eys <eys@google.com>
2019-08-25 14:36:18 +0200

    vector edit

--- Vector.js
+++ Vector.js
@@ -1,23 +1,23 @@
-class Vector2d {
+class Vector {
     constructor(x=0, y=0) {
         this.x = x
         this.y = y
     }
     negate() {
-        return new Vector2d(-this.x, -this.y)
+        return new Vector(-this.x, -this.y)
     }
     add(vector) {
         if (!vector) return 'err: no vector specified'
-        return new Vector2d(this.x + vector.x, this.y + vector.y)
+        return new Vector(this.x + vector.x, this.y + vector.y)
     }
     subtract(vector) {
         return this.add(vector.negate())
     }
     multiply(scalar=1) {
-        return new Vector2d(this.x * scalar, this.y * scalar)
+        return new Vector(this.x * scalar, this.y * scalar)
     }
     divide(scalar=1) {
-        if (scalar === 0) return new Vector2d()
+        if (scalar === 0) return new Vector()
         else              return this.multiply(1/scalar)
     }
     dot(vector) {
@@ -34,4 +34,4 @@ class Vector2d {
 
 /* -------------------------------------------------------------------------------- */
 
-const vec = new Vector2d()
+const vec = new Vector()
