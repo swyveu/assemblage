5cd9ffdb272297d485b5f84bcdaaeb8e68ae083a
eys <eys@google.com>
2019-08-25 13:16:26 +0200

    constructor edit

--- Vector.js
+++ Vector.js
@@ -1,9 +1,11 @@
-
-
 class Vector {
     constructor(x, y) {
         this.x = x
         this.y = y
+    }
+
+    add(vector) {
+        return new Vector(this.x + vector.x, this.y + vector.y)
     }
 }
 
